0ce6c13bec9a37b022225c058a9b94a9c033f675
Ak47u1999 <arpittopper.2017@gmail.com>
2021-11-08 11:58:36 +0530

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ApplicationFormComponent } from './application-form/application-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'PredictionForIssuance',component:ApplicationFormComponent},
+  {path:'Home',component:HomepageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
