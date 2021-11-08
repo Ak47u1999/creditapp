554d0644e5b2dd8d345b8def6e4eede02787ac69
Ak47u1999 <arpittopper.2017@gmail.com>
2021-11-08 10:46:16 +0530

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { ApplicationFormComponent } from './application-form/application-form.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ApplicationFormComponent
+    ApplicationFormComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
