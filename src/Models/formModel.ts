262de41ec51c41564c1aab9f730c4208e8edf4d3
Ak47u1999 <arpittopper.2017@gmail.com>
2021-11-08 12:56:05 +0530

    HomePage Added

---
+++ src/Models/formModel.ts
@@ -0,0 +1,15 @@
+export interface formModel{
+
+    carOwnershipStatus :boolean;
+    homeOwnershipStatus:boolean;
+    numberOfChildren: number;
+    incomeType:string;
+    educationtype:string;
+    totalIncome:number;
+    familyStatus:string;
+    numberOfFamilyMembers:number;
+    housingType:string;
+    age:number;
+    totalYearsEmployed:number;
+    gender:string;
+}
--- src/app/application-form/application-form.component.ts
+++ src/app/application-form/application-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formModel } from 'src/Models/formModel';
 
 @Component({
   selector: 'app-application-form',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApplicationFormComponent implements OnInit {
 
+  
+  formData?:formModel;
   constructor() { }
 
   ngOnInit(): void {
